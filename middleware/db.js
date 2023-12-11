871010788d44be7790acf73149c980acd8da055c
Zas <thezasm03@gmail.com>
2023-12-11 20:45:35 +0100

    Modificacion en masa de usuarios

--- middleware/db.js
+++ middleware/db.js
@@ -2,10 +2,14 @@ const mysql = require("mysql");
 
 const db = mysql.createPool({
     //crear la conexion a la base de datos
+    // host: "161.97.162.29",
+    // user: "chao_chaossuperadmin",
+    // password: "kcuuDPIR@*NUWK7h",
+    // database: "chao_chaos",
     host: "161.97.162.29",
-    user: "chao_chaossuperadmin",
-    password: "kcuuDPIR@*NUWK7h",
-    database: "chao_chaos",
+    user: "chao_chaos",
+    password: "F-7HMvNMF+B^mv@f",
+    database: "chao_chaoss",
 });
 
 module.exports = db;
